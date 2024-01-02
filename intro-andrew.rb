c8a9380687effd1948cd2bfbc3dde78cd721a443
AndrewBalink <andrew.balink@gmail.com>
2024-01-02 15:46:29 -0800

    completed ruby intro challenges

---
+++ intro-andrew.rb
@@ -0,0 +1,77 @@
+# Create a variable and return it in a sentence using string interpolation.
+# create variable
+# age = 30
+
+# # return variable in string interpolation.
+# sentence = "My age is #{age} years old."
+# puts sentence
+
+# Create a variable that contains a string and test some of the Ruby string methods:
+# original_string = "hello, India Cohort!"
+
+# .upcase
+# uppercase_string = original_string.upcase
+# puts "Original string: #{original_string}"
+# puts "Uppercase string: #{uppercase_string}"
+
+# .reverse
+# reversed_string = original_string.reverse
+# puts "Original string: #{original_string}"
+# puts "Reversed string: #{reversed_string}"
+
+# .include?
+# search_term = "Cohort"
+# contains_term = original_string.include?(search_term)
+# puts "Original string: #{original_string}"
+# puts "Does it include '#{search_term}'? #{contains_term}"
+
+# .capitalize
+# capitalized_string = original_string.capitalize
+# puts "Original string: #{original_string}"
+# puts "Capitalizd string: #{capitalized_string}"
+
+# .delete
+# modified_string = original_string.delete(",")
+# puts "Original string: #{original_string}"
+# puts "Modified string: #{modified_string}"
+
+# .index
+# index_of_Cohort = original_string.index("Cohort")
+# puts "Original string: #{original_string}"
+# puts "Index of 'Cohort': #{index_of_Cohort}"
+
+# .swapcase
+# swapped_case_string = original_string.swapcase
+# puts "Original string: #{original_string}"
+# puts "Swapped case string: #{swapped_case_string}"
+
+# Create an array that contains the name of at least five TV shows you enjoy.
+favorite_tv_shows = ["Sons of Anarchy", "Peaky Blinders", "The Sopranos", "Mayans M.C.", "Ozark" ]
+
+# Find the length of the array.
+# length_using_length = favorite_tv_shows.length
+# puts "Length using length method: #{length_using_length}"
+
+# Return the first item in the array.
+# first_show = favorite_tv_shows[0]
+# puts "The first TV show in the array is: #{first_show}"
+
+# Return the fourth item in the array.
+# first_show = favorite_tv_shows[3]
+# puts "The first TV show in the array is: #{first_show}"
+
+# Permanently reverse the order of the array.
+# favorite_tv_shows.reverse!
+# puts "Reversed TV shows array: #{favorite_tv_shows}"
+
+# Create a new empty array for your top favorite TV shows.
+# top_favorite_tv_shows = []
+# top_favorite_tv_shows << "Breaking Bad"
+# top_favorite_tv_shows << "Better Call Saul"
+# top_favorite_tv_shows << "Rescue Me"
+# puts "My top favorite TV shows: #{top_favorite_tv_shows}"
+
+# Using the full TV show array, add your top two favorite shows to the empty array.
+# top_favorite_tv_shows << favorite_tv_shows[0]
+# top_favorite_tv_shows << favorite_tv_shows[1]
+# puts "My top favorite TV shows: #{top_favorite_tv_shows}"
