f69e781531b8a4de7b48eefca72d83539b95e424
Creator912 <walker.joshua912@yahoo.com>
2024-01-03 09:36:07 -0800

    I have it

---
+++ conditionals-methods-jamar-joshua.rb
@@ -0,0 +1,46 @@
+# Create a method called sum_these_numbers which takes two integers as an argument and prints their sum to the screen.
+
+
+
+# def sum_these_numbers(num1, num2)
+#     num1 + num2 
+# end
+
+# p sum_these_numbers(10, 28)
+# ➜  ruby-challenges git:(conditionals-methods-jw-jew) ✗ ruby conditionals-methods-jw1-jew2.rb
+# 38
+# ➜  ruby-challenges git:(conditionals-methods-jw-jew) ✗ 
+
+
+# Create a method called is_even, which takes a single integer, and which then returns true if the number is even, and false otherwise.
+
+def is_even num
+    if num % 2 == 0
+        p 'true'
+    else 
+        p 'false'
+    end
+end
+
+p is_even(44)
+
+# output 
+# ➜  ruby-challenges git:(conditionals-methods-jw-jew) ✗ ruby conditionals-methods-jw1-jew2.rb
+# "true"
+# "true"
+
+# Create a method that takes a number as an argument and prints "Valid" if the number is between 1 and 10 (inclusive) and "Invalid" otherwise.
+
+def 
+    if num % 2 == 0
+        p 'true'
+    else 
+        p 'false'
+    end
+end
+
+p is_even(44)
+
+
+# Create a method that takes in a string and determines if the string is a palindrome.
+
