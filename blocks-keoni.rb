b29c6c90c338007f25b1ffbcbb7ae92ef2639130
Keoni <joonhyuk5972@gmail.com>
2024-01-03 14:35:16 -1000

    a few exercises

---
+++ blocks-keoni.rb
@@ -0,0 +1,46 @@
+#1 Create a loop that prints the numbers 1 to 20.
+# for counting in 1..20
+#     puts counting
+# end
+
+#2 Create a loop that prints the numbers 1 to 20 using a different approach than previously used
+# counter = 1
+#  while counter <= 20
+#     puts counter 
+#     counter =+ 1
+#  end
+
+
+ #3 Create a loop that prints only even numbers from 20 to 0
+# num = 20
+# while num >= 0
+#     p num
+#     num -= 2
+# end
+
+#4 Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5
+# variable_name = [2, 4, 6, 8, 10]
+
+# cooler_variable_name = variable_name.map do |value|
+#     value * 5
+# end
+
+# puts cooler_variable_name
+
+#5 Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+# gas_giants = ['neptume', 'uranus', 'saturn', 'jupiter']
+
+# def upper_case planets
+#     planets.map!(&:capitalize!)
+# end
+
+# upper_case(gas_giants)
+
+# puts gas_giants
+
+#6 Create a method that takes in an array of numbers and returns the largest number in the array
+num_bah = [24, 42, 69, 420, 5318008]
+
+def highest(numbers)
+    highest_number
+end
