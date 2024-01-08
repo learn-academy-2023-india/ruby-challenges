f59678f917e34e1dc5ac317b9e850b8119aff0b0
RobertSarsoza <sarsoza@gmail.com>
2024-01-08 09:30:29 -0800

    still need to do

--- hashes-soza-joshua-tomas.rb
+++ hashes-soza-joshua-tomas.rb
@@ -45,3 +45,25 @@ p my_info.select {|hash, value| value == 30}
 # Remove the key age and its associated value from the my_info variable.
 my_info.delete :age
 p my_info
+
+
+# Create the code that will calculate and log the number of key-value pairs in the my_info variable. (Expected output: 3)
+# Create a custom method called exists that takes the my_info variable and a key as arguments. Return true if the key exists in the hash, otherwise, return false. Use the following method calls to test the functionality.
+# p exists(my_info, 'name')
+# p exists(my_info, 'enjoys')
+# p exists(my_info, 'city')
+# Create a custom method called numeric that takes the my_info variable and returns a hash with only the key-value pairs where the value is numeric.
+# Remove the key age and its associated value from the my_info variable.
+# Cohort Student Numbers
+
+# Create a variable called learn and assign it to an empty hash using the .new method. (Expected output: {})
+# Add the following key-value pairs one at a time to the learn variable: alpha: 20, bravo: 21, charlie: 18, delta: 12, echo: 9, foxtrot: 10
+# Create the code that will iterate through the learn variable and log each key and its corresponding value.
+# Create the code that will iterate through the learn variable and return a hash with only the values that are greater than or equal to 20.
+# Create the code that will iterate through the learn variable and return the sum of all the values. (Expected output: 90)
+# Create a custom method that takes the learn variable as an argument and returns an array containing just the values from the hash.
+# Create a custom method that takes the learn variable as an argument and returns an array containing just the keys from the hash.
+# Create a custom method that takes the learn variable as an argument and returns only the largest value from the hash. (Expected output: 21)
+# Create a custom method that takes the learn variable as an argument and returns only the smallest value from the hash. (Expected output: 9)
+# Create a custom method that takes the learn variable as an argument and returns an array of the values sorted from smallest to largest. (Expected output: [21, 20, 18, 12, 10, 9])
+# Create a custom method that takes the learn variable as an argument and returns a hash of only the key-value pairs where the key starts with a vowel.
